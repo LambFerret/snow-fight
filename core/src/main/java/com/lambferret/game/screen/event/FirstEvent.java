801789d2b223a571c30faa258b90833415790d9e
LambFerret <dignrjrl12@gmail.com>
2023-02-24 15:55:16 +0900

    REFACTORING

--- core/src/main/java/com/lambferret/game/screen/event/FirstEvent.java
+++ core/src/main/java/com/lambferret/game/screen/event/FirstEvent.java
@@ -1,10 +1,8 @@
 package com.lambferret.game.screen.event;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.lambferret.game.component.Dialogue;
 
 public class FirstEvent extends GameEvent {
-    private Dialogue dialogue;
 
     @Override
     public void render(SpriteBatch batch) {
